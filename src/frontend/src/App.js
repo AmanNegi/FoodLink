b95c05097f083452a2cb7998e194639e94e57d44
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-08-12 20:02:29 +0530

    components

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,9 +1,17 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div>
-    
+     <div className="font-righteous">
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path='/' element={} exact/>
+          <Route path='/' element={} exact/> */}
+        </Routes>
+      </BrowserRouter>
+    </div>
     </div>
   );
 }
--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        righteous: ['Righteous', 'cursive'],
+        poppins: ['Poppins', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
