f2e60d83a87664f967cdd1aa5892a5535d3f7d39
Vaishnavi_Kale <vaishnavi.kale3011@gmail.com>
2023-08-12 20:14:15 +0530

    app initialized

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,15 +1,21 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Landing from './components/Landing';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
      <div className="font-righteous">
       <BrowserRouter>
+      <div>
+        <Navbar/>
+      </div>
         <Routes>
-          {/* <Route path='/' element={} exact/>
-          <Route path='/' element={} exact/> */}
+          <Route path='/' element={<Landing/>} exact/>
         </Routes>
+        <Footer/>
       </BrowserRouter>
     </div>
     </div>
